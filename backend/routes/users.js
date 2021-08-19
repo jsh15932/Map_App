ff774b53edc247592d828c6e96e76fa898e24800
jsh15932 <jsh1400512@naver.com>
2021-08-19 20:16:43 +0900

    Add Routers

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
-const User = require("../models/User");
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
 router.post("/register", async (req, res) => {
     try {
